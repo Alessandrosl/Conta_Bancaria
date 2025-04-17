739e4221ab9394171c67558a0e1dc0005bfab59d
2021202060039 <2021202060039@JP-LAB04-02>
2025-04-16 20:50:34 -0400

    Adicionar arquivos de projeto.

---
+++ ContaBancaria.cs
@@ -0,0 +1,10 @@
+﻿
+public class ContaBancaria
+{
+    public int _id;
+    public string _agencia;
+    public string _Nconta;
+    public string _nomeCorrentista;
+    public double _saldo;
+
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        ContaBancaria cb1 = new ContaBancaria();
+        cb1._id = Convert.ToInt32(Console.ReadLine());
+        cb1._agencia = Console.ReadLine();
+        cb1._Nconta = Console.ReadLine();
+        cb1._nomeCorrentista = Console.ReadLine();
+        cb1._saldo = 0;
+    }
+}
